cb1fc39b7f1c7c66fff2fbfc456509b8483a2392
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-02-24 17:31:41 -0700

    Finish chapter 5

--- wizard.lisp
+++ wizard.lisp
@@ -58,3 +58,8 @@
          (push (list object 'body) *object-locations*)
          `(you are now carrying the ,object))
         (t '(you cannot get that.))))
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+
